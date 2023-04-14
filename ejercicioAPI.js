64dcb38916f4dc4810be516aa2538931aecc1f39
Santiago Baliño <santiagobalino@hotmail.com>
2023-04-14 14:07:11 -0300

    Add button to document.write

--- ejercicioAPI.js
+++ ejercicioAPI.js
@@ -1,4 +1,5 @@
 const listaPerros = document.getElementById('lista-perros'); //obtengo mi elemento de HTML y lo guardo en la variable
+
 window.addEventListener('load', () => {
     fetch('https://dog.ceo/api/breeds/list')
         .then((resp) => resp.json())
@@ -9,16 +10,14 @@ window.addEventListener('load', () => {
         .catch((err) => console.log(err));
 });
 
-
 const createCategories = (allBreeds) => {
     const groupArr = groupBreeds(allBreeds);
-    groupArr.map((el, i) => {
-      console.log()
-        const initialLetter = el[0][0].toUpperCase();
+    groupArr.map((breed) => {
+        const initialLetter = breed[0][0].toUpperCase();
         const initial = document.createElement('h2');
         initial.innerText = initialLetter;
         listaPerros.appendChild(initial);
-        el.forEach((raza) => {
+        breed.forEach((raza) => {
             const item = document.createElement('li');
             const link = document.createElement('a');
             link.innerText = raza.charAt(0).toUpperCase() + raza.slice(1);
@@ -40,9 +39,13 @@ const mostrarFotoPerro = async (raza) => {
 
         const nuevaVentana = window.open(); // creo una nueva ventana
         nuevaVentana.document.write(`<div style="text-align: center;">
+        <a href="http://127.0.0.1:5500/" style=
+        "position: absolute; top: 10; left: 10; background: teal; text-decoration: none; border: 2px solid teal; color: white; border-radius: 8px; padding: 4px 8px; cursor: pointer; outline: inherit;"
+        ;">test</a>
   <img src="${data.message}" alt="${raza}" style="border: 1px solid black;">
   <h1 style="font-family: 'Bebas Neue', cursive;  text-transform: uppercase; color: #5a382c;">${raza}</h1>
-</div>`); // creo un div donde almaceno la img y el nombre de la raza
+</div>`);
+        attach(); // creo un div donde almaceno la img y el nombre de la raza
     } catch (error) {
         console.log(error);
     }
@@ -61,3 +64,11 @@ const groupBreeds = (arr) => {
 
     return groups;
 };
+
+function attach() {
+    const backButton = document.getElementById('back-button');
+
+    backButton.addEventListener('click', function () {
+        onclick = "window.location.href='http://127.0.0.1:5500/#'";
+    });
+}
